f9c854d16143b454ea65aad809cd0465340ab87a
ehi23 <158773668+ehi23@users.noreply.github.com>
2024-02-21 09:52:29 -0500

    Add files via upload

---
+++ hiv infection.sql
@@ -0,0 +1,30 @@
+select*
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+
+
+select*
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+where Entity = 'Nigeria'
+
+select top (5) Entity [country], year,[Children ages( 0-14) newly infected with HIV]
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+where Entity = 'Nigeria'
+
+select count([Children ages( 0-14) newly infected with HIV])
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+where Entity = 'Nigeria'
+
+select avg([Children ages( 0-14) newly infected with HIV])
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+where Entity = 'Nigeria'
+
+select min([Children ages( 0-14) newly infected with HIV])
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+where Entity = 'Nigeria'
+
+select max([Children ages( 0-14) newly infected with HIV])
+from [portfolioproject1].[dbo].['number-of-children-newly-infect$']
+where Entity = 'Nigeria'
+
+
+
